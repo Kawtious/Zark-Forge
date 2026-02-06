2a813da719b3f675bc0ba02b7e472548b6c3f787
Kawtious <mewmewby2@gmail.com>
2026-02-05 20:25:49 -0700

    Add TailwindCSS to the project

--- src/views/ForgeView.vue
+++ src/views/ForgeView.vue
@@ -8,78 +8,27 @@ import ForgeSocials from '@/components/ForgeSocials.vue';
 <template>
   <ForgeBackground />
 
-  <div class="view">
-    <section class="left">
+  <div class="flex min-h-screen flex-col lg:flex-row">
+    <section
+      class="mx-auto flex w-full flex-col justify-center gap-6 px-[clamp(2rem,6vw,12rem)] py-[clamp(4rem,8vh,8rem)] lg:mx-0 lg:w-1/2"
+    >
       <ForgeTitle title="The Forge Awakens" />
 
-      <p class="description">
-        Sparks are flying and the anvil is ringing; this corner of the forge is still being
-        shaped.<br /><br />
-        I'm hammering ideas into steel, tempering designs with fire, and making sure everything that
-        comes out is worthy of a dragon's seal.<br /><br />
-        Check back soon to see what emerges from the flames.
-      </p>
+      <div class="flex flex-col gap-6 lg:max-w-prose">
+        <p class="text-base leading-relaxed text-[#e6e2df]">
+          Sparks are flying and the anvil is ringing; this corner of the forge is still being
+          shaped.<br /><br />
+          I'm hammering ideas into steel, tempering designs with fire, and making sure everything
+          that comes out is worthy of a dragon's seal.<br /><br />
+          Check back soon to see what emerges from the flames.
+        </p>
 
-      <ForgeSocials />
+        <ForgeSocials />
+      </div>
     </section>
 
-    <section class="right">
+    <section class="flex shrink-0 items-center justify-center lg:w-1/2">
       <ForgeDragon />
     </section>
   </div>
 </template>
-
-<style scoped>
-.view {
-  min-height: 100vh;
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(0, 1fr));
-  color: #e6e2df;
-}
-
-.left {
-  display: flex;
-  flex-direction: column;
-
-  padding: 0 10rem 0 10rem;
-  gap: 1.5rem;
-
-  justify-content: center;
-}
-
-.right {
-  display: flex;
-
-  align-items: center;
-  justify-content: center;
-}
-
-.description {
-  line-height: 1.5;
-}
-
-@media (max-width: 1280px) {
-  .left {
-    padding-right: 6rem;
-    padding-left: 6rem;
-  }
-}
-
-@media (max-width: 1024px) {
-  .view {
-    grid-template-columns: 1fr;
-  }
-
-  .left {
-    padding-top: 4rem;
-    padding-bottom: 4rem;
-  }
-}
-
-@media (max-width: 900px) {
-  .left {
-    padding-right: 2rem;
-    padding-left: 2rem;
-  }
-}
-</style>
