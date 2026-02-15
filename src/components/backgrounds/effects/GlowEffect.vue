55d28edb95a1f5f736e4b95ef366deadb37523ec
Kawtious <mewmewby2@gmail.com>
2026-02-15 13:49:32 -0700

    Add glow pulse effect to background

--- src/components/backgrounds/effects/GlowEffect.vue
+++ src/components/backgrounds/effects/GlowEffect.vue
@@ -1,3 +1,5 @@
-<script setup lang="ts"></script>
-
-<template></template>
+<template>
+  <div
+    class="pointer-events-none fixed bottom-[-90%] left-1/2 -translate-x-1/2 w-[180vw] h-[120vh] bg-[radial-gradient(ellipse_at_center,rgba(255,120,0,0.6)_0%,rgba(255,80,0,0.35)_30%,rgba(255,50,0,0.15)_50%,transparent_70%)] blur-3xl opacity-70 animate-glow-pulse"
+  />
+</template>
