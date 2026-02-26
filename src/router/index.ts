b9c5c2fa27d5ad8065180593c64a13b85b2adb34
Kawtious <mewmewby2@gmail.com>
2026-02-25 20:33:53 -0700

    Add empty About page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
+import HomeView from '@/views/HomeView.vue';
 import { createRouter, createWebHistory } from 'vue-router';
-
-import HomeView from '../views/HomeView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +8,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('@/views/AboutView.vue')
     }
   ]
 });
