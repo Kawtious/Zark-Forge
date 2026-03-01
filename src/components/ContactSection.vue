2b91fd4f087ea27d2e70b6976a7eb9bcdef9fa45
Kawtious <mewmewby2@gmail.com>
2026-02-28 23:13:07 -0700

    Add Twitter button

--- src/components/ContactSection.vue
+++ src/components/ContactSection.vue
@@ -2,6 +2,7 @@
 import IconDiscord from '@/components/icons/IconDiscord.vue';
 import IconEmail from '@/components/icons/IconEmail.vue';
 import IconGitHub from '@/components/icons/IconGitHub.vue';
+import IconTwitter from '@/components/icons/IconTwitter.vue';
 import { useCopyTooltipsStore } from '@/stores/copy-tooltips.ts';
 
 const { copyTooltips } = useCopyTooltipsStore();
@@ -42,6 +43,19 @@ function copy(text: string, event: MouseEvent) {
         colour="currentColor"
       />
     </button>
+
+    <a
+      target="_blank"
+      rel="noopener noreferrer"
+      href="https://x.com/zashveil"
+      aria-label="Twitter"
+      class="group flex items-center justify-center gap-2 rounded-xl px-3 py-2 text-sm font-semibold text-title-1 transition duration-200 hover:-translate-y-0.5 focus:outline-none focus-visible:ring-2 focus-visible:ring-title-2/50"
+    >
+      <IconTwitter
+        class="h-10 w-10 shrink-0 text-title-2 transition duration-200 group-hover:drop-shadow-[0_0_6px_rgba(255,80,0,0.8)]"
+        colour="currentColor"
+      />
+    </a>
 
     <a
       href="mailto:contact@zarkforge.xyz"
